0403b8168032bf92ab6bf49ce6839d5628c19f8f
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-09 22:32:58 -0500

    adds logout comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Login from './user/Login'
+import Logout from './user/Logout'
 
 export default function App() {
-  return <Login />
+  return <Logout user="Frank Pigeon" />
 }
