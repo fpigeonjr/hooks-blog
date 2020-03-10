b79e41b3526317256dc33e7b5af6f38d2d8ad5ab
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-09 22:39:16 -0500

    adds Register comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Logout from './user/Logout'
+import Register from './user/Register'
 
 export default function App() {
-  return <Logout user="Frank Pigeon" />
+  return <Register />
 }
