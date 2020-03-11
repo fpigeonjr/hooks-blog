f949b4c9fa75782c884d4ed0cbc6bbfdc0567994
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-10 22:38:06 -0500

    adds Post comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import UserBar from './user/UserBar'
+import Post from './post/Post'
 
 export default function App() {
-  return <UserBar />
+  return (
+    <>
+      <UserBar />
+      <Post title="React Hooks" content="The greatest things since sliced bread!" author="Frank Pigeon" />
+    </>
+  )
 }
