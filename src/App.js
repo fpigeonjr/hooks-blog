047ebe32db2ac2cb381f9778e35c16776a562c5f
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-10 22:43:43 -0500

    adds Create Post comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import UserBar from './user/UserBar'
 import Post from './post/Post'
+import CreatePost from './post/CreatePost'
 
 export default function App() {
   return (
     <>
       <UserBar />
       <Post title="React Hooks" content="The greatest things since sliced bread!" author="Frank Pigeon" />
+      <CreatePost />
     </>
   )
 }
