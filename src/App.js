c9140fbeb08572ea54863044e817721eaafa1379
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-11 00:01:21 -0500

    passes user to create post

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserBar from './user/UserBar'
 import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 
-const user = 'Frank Pigeon'
-const posts = [
-  {
-    title: 'React Hooks',
-    content: 'The greatest things since sliced bread!',
-    author: 'Frank Pigeon'
-  },
-  {
-    title: 'Using React Fragments',
-    content: 'Keeping the DOM Clean',
-    author: 'Frank Pigeon'
-  }
-]
 export default function App() {
+  const posts = [
+    {
+      title: 'React Hooks',
+      content: 'The greatest things since sliced bread!',
+      author: 'Frank Pigeon'
+    },
+    {
+      title: 'Using React Fragments',
+      content: 'Keeping the DOM Clean',
+      author: 'Frank Pigeon'
+    }
+  ]
+  const [user, setUser] = useState('')
   return (
     <div style={{ padding: 8 }}>
-      <UserBar />
-      <br/>
-      <CreatePost user={user}/>
-      <br/>
-      <hr/>
+      <UserBar user={user} setUser={setUser} />
+      <br />
+      {user && <CreatePost user={user} />}
+      <br />
+      <hr />
       <PostList posts={posts} />
     </div>
   )
--- src/user/UserBar.js
+++ src/user/UserBar.js
@@ -4,8 +4,7 @@ import Login from './Login'
 import Logout from './Logout'
 import Register from './Register'
 
-export default function UserBar() {
-  const [user, setUser] = useState('')
+export default function UserBar({ user, setUser }) {
   if (user) {
     return <Logout user={user} setUser={setUser} />
   } else {
