ae3b031570b3ea16cdeab745a42a31f35558d189
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-10 22:58:20 -0500

    adds PostList and modifies the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import UserBar from './user/UserBar'
-import Post from './post/Post'
+import PostList from './post/PostList'
 import CreatePost from './post/CreatePost'
 
+const user = 'Frank Pigeon'
+const posts = [
+  {
+    title: 'React Hooks',
+    content: 'The greatest things since sliced bread!',
+    author: 'Frank Pigeon'
+  },
+  {
+    title: 'Using React Fragments',
+    content: 'Keeping the DOM Clean',
+    author: 'Frank Pigeon'
+  }
+]
 export default function App() {
   return (
-    <>
+    <div style={{ padding: 8 }}>
       <UserBar />
-      <Post title="React Hooks" content="The greatest things since sliced bread!" author="Frank Pigeon" />
-      <CreatePost />
-    </>
+      <br/>
+      <CreatePost user={user}/>
+      <br/>
+      <hr/>
+      <PostList posts={posts} />
+    </div>
   )
 }
