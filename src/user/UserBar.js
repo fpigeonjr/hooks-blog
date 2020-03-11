638049bf368a7211229933d7b4e990b3e7a600dc
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-03-10 23:37:31 -0500

    adds functionailty to the login

--- src/user/UserBar.js
+++ src/user/UserBar.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Login from './Login'
 import Logout from './Logout'
 import Register from './Register'
 
 export default function UserBar() {
-  const user = 'Frank Pigeon'
+  const [user, setUser] = useState('')
   if (user) {
-    return <Logout user={user} />
+    return <Logout user={user} setUser={setUser} />
   } else {
     return (
       <>
-        <Login />
-        <Register />
+        <Login setUser={setUser} />
+        <Register setUser={setUser} />
       </>
     )
   }
